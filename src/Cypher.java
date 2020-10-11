3cfb48aa580ef2c5cace12e665f2596da3fb07dd
Shosuke Noma <snoma@macalester.edu>
2020-10-11 15:18:03 +0900

    Create Overall Structure of main class

---
+++ src/Cypher.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Cypher {
+
+    public Cypher (){
+        // Constructor
+    }
+
+    private String encypher(String text) {
+        return "Implementation coming soon :)";
+    }
+
+    public static void main(String[] arg) {
+        Cypher cypher = new Cypher();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the text you want to encode: ");
+        String text = scanner.next();
+        System.out.println(cypher.encypher(text));
+    }
+}
