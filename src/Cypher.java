faec664d45d7f63472401f138f53d2463a3aeb3f
andiewalker <awalker3@macalester.edu>
2020-10-12 12:52:33 -0600

    Implemented the encypher method to use a simple Caesar cipher with an int as a key that shifts the alphabet.

--- src/Cypher.java
+++ src/Cypher.java
@@ -1,24 +1,49 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Cypher {
 
-    //test
+    String[] alphabet;
+
     public Cypher (){
         // Constructor
+        alphabet = new String[]{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+        "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
     }
 
-    private String encypher(String text){
+    private String encypher(String text, int key){
+        text = text.trim();
+        text = text.toLowerCase();
+        char[] chars = text.toCharArray();
+        key = key % 26;
+        Map charMap = getCaesarMap(key);
+        StringBuilder result = new StringBuilder();
+        for(char letter : chars){
+            if(letter >= 'a' && letter <= 'z') {
+                result.append(charMap.get(letter));
+            }else{
+                result.append(letter);
+            }
+        }
+        return (result.toString());
+    }
 
-
-        return "Implementation coming soon :)";
-
+    private Map<Character, Character> getCaesarMap(int key) {
+        Map<Character, Character> charMap = new HashMap<>();
+        for(int i = 0; i < 26; i ++){
+            charMap.put(alphabet[i].charAt(0), alphabet[(i + key) % 26].charAt(0));
+        }
+        return charMap;
     }
 
     public static void main(String[] arg) {
         Cypher cypher = new Cypher();
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the text you want to encode: ");
-        String text = scanner.next();
-        System.out.println(cypher.encypher(text));
+        String text = scanner.nextLine();
+        System.out.println("Enter the key you want to use for encoding: ");
+        int key = scanner.nextInt();
+        System.out.println(cypher.encypher(text, key));
     }
 }
